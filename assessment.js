046c234e5753c4838c4e255709c717937c419cd2
yanchan37 <77221532+yanchan37@users.noreply.github.com>
2021-03-05 23:26:15 +0900

    kidousinakunattanode

--- assessment.js
+++ assessment.js
@@ -3,13 +3,14 @@ const userNameInput = document.getElementById('user-name');
 const assessmentButton = document.getElementById('assessment');
 const resultDivided = document.getElementById('result-area');
 const tweetDivided = document.getElementById('tweet-area');
+
 /**
- * 指定した要素の子どもを全て削除する
+ * 指定した要素の子どもを全て除去する
  * @param {HTMLElement} element HTMLの要素
  */
 function removeAllChildren(element) {
   while (element.firstChild) {
-    // 子どもの要素があるかぎり削除
+    // 子どもの要素があるかぎり除去
     element.removeChild(element.firstChild);
   }
 }
@@ -32,7 +33,7 @@ assessmentButton.onclick = () => {
   paragraph.innerText = result;
   resultDivided.appendChild(paragraph);
 
-  // TODO ツイートエリアの作成
+  // ツイートエリアの作成
   removeAllChildren(tweetDivided);
   const anchor = document.createElement('a');
   const hrefValue =
@@ -49,10 +50,10 @@ assessmentButton.onclick = () => {
   const script = document.createElement('script');
   script.setAttribute('src', 'https://platform.twitter.com/widgets.js');
   tweetDivided.appendChild(script);
-  };
+};
 
 const answers = [
-  '{userName}のいいところは声です。{userName}の特徴的な声は皆を惹きつけ、心に残ります。',
+  '{userName}のいいところは声です。{userName}の特徴的な声はみなを惹きつけ、心に残ります。',
   '{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。',
   '{userName}のいいところは情熱です。{userName}の情熱に周りの人は感化されます。',
   '{userName}のいいところは厳しさです。{userName}の厳しさがものごとをいつも成功に導きます。',
@@ -68,7 +69,7 @@ const answers = [
   '{userName}のいいところは気配りです。{userName}の配慮が多くの人を救っています。',
   '{userName}のいいところはその全てです。ありのままの{userName}自身がいいところなのです。',
   '{userName}のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる{userName}が皆から評価されています。'
-  '{userName}のいいところは優しさです。あなたの優しい雰囲気や立ち振る舞いに多くの人が癒やされています。'
+  '{userName}のいいところは優しさです。{userName}の優しい雰囲気や立ち振る舞いに多くの人が癒やされています。'
 ];
 
 /**
@@ -78,24 +79,18 @@ const answers = [
  */
 function assessment(userName) {
   // 全文字のコード番号を取得してそれを足し合わせる
-  let sumOfCharCode = 0;
+  let sumOfcharCode = 0;
   for (let i = 0; i < userName.length; i++) {
-    sumOfCharCode = sumOfCharCode + userName.charCodeAt(i);
+    sumOfcharCode = sumOfcharCode + userName.charCodeAt(i);
   }
 
   // 文字のコード番号の合計を回答の数で割って添字の数値を求める
-  const index = sumOfCharCode % answers.length;
+  const index = sumOfcharCode % answers.length;
   let result = answers[index];
 
-  result = result.replace(/\{userName\}/g, userName);
+  result = result.replace(/{userName}/g, userName);
   return result;
 }
-userNameInput.onkeydown = event => {
-  if (event.key === 'Enter') {
-    // TODO ボタンのonclick() 処理を呼び出す
-    assessmentButton.onclick();
-  }
-};
 
 // テストコード
 console.assert(
